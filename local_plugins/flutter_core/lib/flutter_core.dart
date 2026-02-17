bbc018794a05bd44890671beddcf5b8f8940d6f1
ivomika <67991025+ivomika@users.noreply.github.com>
2026-02-17 16:02:20 +0300

    add other libs

--- local_plugins/flutter_core/lib/flutter_core.dart
+++ local_plugins/flutter_core/lib/flutter_core.dart
@@ -1,2 +1,8 @@
 export './api/api.dart';
 export './logger/logger.dart';
+export 'package:dio/dio.dart';
+export 'package:talker/talker.dart';
+export 'package:equatable/equatable.dart';
+export 'package:json_annotation/json_annotation.dart';
+export 'package:freezed_annotation/freezed_annotation.dart';
+export 'package:shared_preferences/shared_preferences.dart';
